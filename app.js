cfc41a450f088e83cc92e47ec9a4c743ecf55f8d
samritibajaj <smritibajaj00@gmail.com>
2024-09-08 20:24:50 +0530

    files updated

--- app.js
+++ app.js
@@ -1,15 +1,62 @@
-let btn=document.querySelector(".search-btn");
 
-let apiKey= "8589ae2cca137122b055347b08f319d6";
-let apiURL= `https://api.openweathermap.org/data/2.5/weather?q=chandigarh&appid=${apiKey}&units=metric`
+//  let apiKey= "8589ae2cca137122b055347b08f319d6";
+//  let apiURL= `https://api.openweathermap.org/data/2.5/weather?q=&appid=${apiKey}&units=metric`
+let apiURL= "https://api.openweathermap.org/data/2.5/weather?appid=8589ae2cca137122b055347b08f319d6&units=metric&q="
+
+let input=document.querySelector(".search input")
+let btn=document.querySelector(".search-btn")
 
 
 
-let checkweather= async()=>{
-  let response=await fetch(apiURL)
-  // console.log(response);
-  let data= await response.json();
+ let checkweather= async(city)=>{
+  let response=await fetch(apiURL +city);
+  console.log(response);
+  let data=await response.json();
   console.log(data);
   
+  document.querySelector(".city").innerHTML= data.name
+document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+ "°C"
+
+document.querySelector(".humid-p").innerHTML= data.main.humidity + "%"
+document.querySelector(".wind-p").innerHTML= data.wind.speed + "km/hr"
+
 }
+
+btn.addEventListener('click', ()=>{
+  checkweather(input.value)
+})
+
+
+
+
+// let apiKey= "8589ae2cca137122b055347b08f319d6";
+// let apiURL= `https://api.openweathermap.org/data/2.5/weather?q=&appid=${apiKey}&units=metric`
+
+// // let input=document.querySelector(".search input")
+// // let btn=document.querySelector(".search-btn")
+
+
+
+// let checkweather= async()=>{
+//   let response=await fetch(apiURL )
+//   // console.log(response);
+//   let data= await response.json();
+//   console.log(data);
+
+
+//   // document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+ "°C"
+//   // document.querySelector(".city").innerHTML= data.name
+//   // document.querySelector(".humid-p").innerHTML= data.main.humidity + "%"
+//   // document.querySelector(".wind-p").innerHTML= data.wind.speed + "km/hr"
+ 
+// }
+
+// // btn.addEventListener("click", ()=>{
+// //   checkweather(input.value)
+// // })
+
+
+
+
+// // checkweather();
 
